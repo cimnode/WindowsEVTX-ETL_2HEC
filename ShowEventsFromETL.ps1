7506f8c02a6199215ee38f43ce0b2335d854910a
cimnode <49315989+cimnode@users.noreply.github.com>
2022-01-07 09:16:54 -0500

    Delete ShowEventsFromETL.ps1

--- ShowEventsFromETL.ps1
+++
@@ -1,7 +0,0 @@
-#This file fill read 1 minute of logs starting 2 minutes ago.
-$args = @{}
-$args.Add("StartTime", ((Get-Date).AddMinutes(-2)))
-$args.Add("EndTime", ((Get-Date).AddMinutes(-1)))
-$args.Add("Path", "c:\windows\System32\Winevt\Logs\Microsoft-Windows-DNSServer-Analytical.etl")
-
-Get-WinEvent -FilterHashtable $args -oldest
